1a703fb1f6af6f7b9103265c043213a609124fb2
shahin hasanov <shahinium888@gmail.com>
2022-11-17 18:32:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Cinema_projects
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btnchanged = new System.Windows.Forms.Button();
+            this.btnabouts = new System.Windows.Forms.Button();
+            this.btnfullsc = new System.Windows.Forms.Button();
+            this.txtlink = new System.Windows.Forms.TextBox();
+            this.txtcatog = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1381, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Ravie", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(779, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CINEMA PROJECT TEST";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1062, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.btnsave);
+            this.groupBox2.Controls.Add(this.groupBox5);
+            this.groupBox2.Controls.Add(this.txtlink);
+            this.groupBox2.Controls.Add(this.txtcatog);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtname);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 575);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "NEW FILM";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(124, 158);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(172, 66);
+            this.btnsave.TabIndex = 8;
+            this.btnsave.Text = "SAVE";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.btnexit);
+            this.groupBox5.Controls.Add(this.btnchanged);
+            this.groupBox5.Controls.Add(this.btnabouts);
+            this.groupBox5.Controls.Add(this.btnfullsc);
+            this.groupBox5.Location = new System.Drawing.Point(6, 308);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(284, 261);
+            this.groupBox5.TabIndex = 7;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Transactions";
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(6, 183);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(258, 48);
+            this.btnexit.TabIndex = 9;
+            this.btnexit.Text = "EXIT";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // btnchanged
+            // 
+            this.btnchanged.Location = new System.Drawing.Point(6, 129);
+            this.btnchanged.Name = "btnchanged";
+            this.btnchanged.Size = new System.Drawing.Size(258, 48);
+            this.btnchanged.TabIndex = 8;
+            this.btnchanged.Text = "CHANGED BACKCOLORS";
+            this.btnchanged.UseVisualStyleBackColor = true;
+            this.btnchanged.Click += new System.EventHandler(this.btnchanged_Click);
+            // 
+            // btnabouts
+            // 
+            this.btnabouts.Location = new System.Drawing.Point(6, 75);
+            this.btnabouts.Name = "btnabouts";
+            this.btnabouts.Size = new System.Drawing.Size(258, 48);
+            this.btnabouts.TabIndex = 7;
+            this.btnabouts.Text = "ABOUTS";
+            this.btnabouts.UseVisualStyleBackColor = true;
+            this.btnabouts.Click += new System.EventHandler(this.btnabouts_Click);
+            // 
+            // btnfullsc
+            // 
+            this.btnfullsc.Location = new System.Drawing.Point(6, 21);
+            this.btnfullsc.Name = "btnfullsc";
+            this.btnfullsc.Size = new System.Drawing.Size(258, 48);
+            this.btnfullsc.TabIndex = 6;
+            this.btnfullsc.Text = "FULL SCREEN";
+            this.btnfullsc.UseVisualStyleBackColor = true;
+            this.btnfullsc.Click += new System.EventHandler(this.btnfullsc_Click);
+            // 
+            // txtlink
+            // 
+            this.txtlink.Location = new System.Drawing.Point(124, 130);
+            this.txtlink.Name = "txtlink";
+            this.txtlink.Size = new System.Drawing.Size(166, 22);
+            this.txtlink.TabIndex = 5;
+            // 
+            // txtcatog
+            // 
+            this.txtcatog.Location = new System.Drawing.Point(124, 87);
+            this.txtcatog.Name = "txtcatog";
+            this.txtcatog.Size = new System.Drawing.Size(166, 22);
+            this.txtcatog.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Marker", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Brown;
+            this.label5.Location = new System.Drawing.Point(33, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Film Link:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Marker", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Brown;
+            this.label4.Location = new System.Drawing.Point(2, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "FilmCatogory:";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(124, 47);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(166, 22);
+            this.txtname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Marker", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(20, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Film Name:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.webBrowser1);
+            this.groupBox3.Location = new System.Drawing.Point(651, 118);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(742, 575);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "SCREEN";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 18);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(736, 554);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DockChanged += new System.EventHandler(this.webBrowser1_DockChanged);
+            this.webBrowser1.Move += new System.EventHandler(this.webBrowser1_Move);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Controls.Add(this.dataGridView1);
+            this.groupBox4.Location = new System.Drawing.Point(314, 118);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(319, 575);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "FILM LIST";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(313, 554);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1417, 705);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Button btnchanged;
+        private System.Windows.Forms.Button btnabouts;
+        private System.Windows.Forms.Button btnfullsc;
+        private System.Windows.Forms.TextBox txtlink;
+        private System.Windows.Forms.TextBox txtcatog;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+
+namespace Cinema_projects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection bagla = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\SHAKHAE\Documents\FILMLER.accdb");
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        void films()
+        {
+            OleDbDataAdapter db = new OleDbDataAdapter("Select * from TBL_FILMLER", bagla);
+            DataTable dt = new DataTable();
+            db.Fill(dt);
+            dataGridView1.DataSource = dt;
+            
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            films();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            bagla.Open();
+            OleDbCommand kt1 = new OleDbCommand("Insert into TBL_FILMLER (AD,KATOGORI,LINK) VALUES (@p1,@p2,@p3)", bagla);
+            kt1.Parameters.AddWithValue("@p1", txtname.Text);
+            kt1.Parameters.AddWithValue("@p2", txtcatog.Text);
+            kt1.Parameters.AddWithValue("@p3", txtlink.Text);
+            kt1.ExecuteNonQuery();
+            bagla.Close();
+            MessageBox.Show("Lucky is add new film(*_*)");
+            films();
+                
+         }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            string link = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            webBrowser1.Navigate(link);
+        }
+
+        private void btnabouts_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This project was coded by Shahin Hassan on November 9th.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        void color()
+
+        {
+            int r, g, b;
+            Random rd = new Random();
+            r = rd.Next(250);
+            g = rd.Next(250);
+            b = rd.Next(250);
+            this.BackColor = Color.FromArgb(r, g, b);
+
+
+        }
+        private void btnchanged_Click(object sender, EventArgs e)
+        {
+            color();
+        }
+       
+        private void btnfullsc_Click(object sender, EventArgs e)
+        {
+            groupBox3.Dock = DockStyle.Fill;
+            groupBox4.Hide();
+
+
+
+
+        }
+
+        public void webBrowser1_DockChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void webBrowser1_Move(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            groupBox3.Size =this.Size;
+        }
+    }
+}
